8c499b43255856f04959a8787b5a87d9a5a6ec49
iuhoosa <32049895+iuhoosa@users.noreply.github.com>
2018-01-05 22:36:37 +0900

    Add files via upload
    Made during fact project.

---
+++ createTower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createTower : MonoBehaviour {
+
+    public GameObject tower;
+    public bool isClicked = false;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isClicked && Input.GetMouseButton(0))
+        {
+            crateTower();
+        }
+    }
+
+    public void buttonClick()
+    {
+        isClicked = true;
+    }
+
+    void crateTower()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 offset = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        GameObject newtower = (GameObject)Instantiate(tower) as GameObject;
+        
+        newtower.transform.position = new Vector2(offset.x,offset.y);
+        newtower.GetComponent<SpriteRenderer>().enabled = true;
+        isClicked = false;
+    }
+}
---
+++ makeTower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeTower : MonoBehaviour
+{
+    public GameObject tower;
+   
+    void Start()
+    {
+        maketower();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void maketower()
+    {
+        GameObject newtower = (GameObject)Instantiate(tower) as GameObject;
+        float x = Random.Range(-25, 25);
+        float y = Random.Range(-25, 25);
+        newtower.transform.position = new Vector2(x, y);
+    }
+
+    void maketower2()
+    {
+        GameObject myRoadInstance =
+            Instantiate(Resources.Load("tower"),
+            new Vector3(5, 5, 0),
+            Quaternion.identity) as GameObject;
+        Instantiate(Resources.Load("tower"), new Vector3(3, 3, 0), Quaternion.identity);
+
+    }
+}
